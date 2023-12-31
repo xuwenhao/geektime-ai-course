6825e05da76e1d435d66a7d96f02ef55ad1bfd7a
Xu Wenhao <xuwenhao@shoppal.ai>
2023-12-31 09:20:32 +0000

    move venv creation to Docker rather than postCreateCommand script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-python -m venv ~/venv
-echo "source ~/venv/bin/activate" >> ~/.zshrc
 source /home/vscode/venv/bin/activate
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -r requirements.txt
