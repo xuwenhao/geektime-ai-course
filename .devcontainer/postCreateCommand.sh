032cc819ead02333108a5aa15bc5a3210cc2bd2f
Xu Wenhao <xuwenhao@shoppal.ai>
2023-12-31 03:22:03 +0000

    using dev container for better isolated dev environment

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,7 @@
-sudo pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-sudo pip install -r requirements.txt
+#!/bin/bash
+python -m venv ~/venv
+echo "source ~/venv/bin/activate" >> ~/.zshrc
+source /home/vscode/venv/bin/activate
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -r requirements.txt
+sudo chown vscode:vscode /workspace
